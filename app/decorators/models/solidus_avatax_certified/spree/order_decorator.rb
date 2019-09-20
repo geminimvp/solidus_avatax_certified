5605ce1bd4516fe823ad029583daa24321b0993d
fastjames <fastjames@gmail.com>
2019-09-20 14:01:12 -0500

    Tolerate missing config in address validation

--- app/decorators/models/solidus_avatax_certified/spree/order_decorator.rb
+++ app/decorators/models/solidus_avatax_certified/spree/order_decorator.rb
@@ -15,7 +15,7 @@ module SolidusAvataxCertified
       end
 
       def avalara_tax_enabled?
-        SolidusAvataxCertified::Current.config.tax_calculation
+        SolidusAvataxCertified::Current.config&.tax_calculation
       end
 
       def cancel_avalara
