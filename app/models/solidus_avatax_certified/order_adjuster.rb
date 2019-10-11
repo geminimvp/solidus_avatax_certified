742023cb69501c2cabe99c4cdef23cc51440b793
fastjames <fastjames@gmail.com>
2019-10-11 14:51:43 -0500

    Ensure module reference is absolute
    In some cases, Ruby gets confused about the reference to the parent
    class here, so we make it an absolute reference to avoid any confusion.

--- app/models/solidus_avatax_certified/order_adjuster.rb
+++ app/models/solidus_avatax_certified/order_adjuster.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SolidusAvataxCertified
-  class OrderAdjuster < Spree::Tax::OrderAdjuster
+  class OrderAdjuster < ::Spree::Tax::OrderAdjuster
     def adjust!
       if order.avalara_tax_enabled? &&
           %w(cart address delivery).include?(order.state)
